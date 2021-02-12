401bdb26590cdd4acadbf6836a5c2242872acf69
Kari-Thrains <kat22@hi.is>
2021-02-12 17:09:22 +0000

    Fyrsta utgafa

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const {
-  PORT: port = 3000,
-} = process.env;
 
 const app = express();
 
-// TODO setja upp rest af virkni!
+app.use(express.urlencoded({ extended: true }));
 
-// Verðum að setja bara *port* svo virki á heroku
-app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}/`);
+app.get('/', (req, res) => {
+  res.send(`
+<form method="post" action="/post" enctype="application/x-www-form-urlencoded">
+  <input type="text" name="data">
+  <input type="file" name="file">
+  <button>Senda</button>
+</form>
+  `);
+});
+
+app.post('/post', (req, res) => {
+  res.send(`POST gögn: ${JSON.stringify(req.body)}`);
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
